bd60ff1ec4bc46fc5f452c9cb32f748d5fb5f085
James Jeffreys <jeffreysjames87@gmail.com>
2022-08-17 14:23:21 +0100

    added middleware

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ dbConnectionString = process.env.DB_STRING,
 dbname = 'kruger-park-db',
 collection
 
+//Connect Database
 MongoClient.connect(dbConnectionString)
 .then(client => {
     console.log('Connected to DB')
     db = client.db(dbname)
     collection = db.collection('animals')
 })
+
+//Middleware
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+//Body parser is no longer needed. Express has built in it's fucntionality
+app.use(cors())
 
 app.listen(process.env.PORT|| PORT, ()=>{
     console.log(`Server running on Port ${process.env.PORT}`)
