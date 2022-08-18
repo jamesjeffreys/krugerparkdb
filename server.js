e9acfbc73e969966431b7584621ee9259c40ff5e
James Jeffreys <jeffreysjames87@gmail.com>
2022-08-18 18:43:06 +0100

    put routes in mongoclient

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ dbConnectionString = process.env.DB_STRING,
 dbname = 'kruger-park-db',
 collection
 
-//Connect Database
-MongoClient.connect(dbConnectionString)
-.then(client => {
-    console.log('Connected to DB')
-    db = client.db('kruger-national-park')
-    collection = db.collection('animals')
-})
-
 //Middleware
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -25,62 +17,70 @@ app.use(express.json())
 //Body parser is no longer needed. Express has built in it's fucntionality directly.
 app.use(cors())
 
+//Connect Database
+MongoClient.connect(dbConnectionString)
+.then(client => {
+    console.log('Connected to DB')
+    db = client.db('kruger-national-park')
+    collection = db.collection('animals')
 //Routes
 
-//GET
+    //GET
 app.get('/', (req, res) => {
-    db.collection('animals').find().toArray()
-      .then(results => {
-      res.render('index.ejs', {animals: results})
-      })
-      .catch(error => console.error(error))
-    
-  })
+  db.collection('animals').find().toArray()
+    .then(results => {
+    res.render('index.ejs', {animals: results})
+    })
+    .catch(error => console.error(error))
+  
+})
 
 //API Route
 
-app.get('/api/:animalName', (request, response) => {
-  const animalName = request.params.animalName.toLowerCase()
-  if(collection[animalName]){
-      response.json(collection[animalName])
-  } else {
-      response.json(collection['Animal not in the Kruger'])
-  }
-})
+    app.get('/api/:alienName', (request, response) => {
+      const animalName = request.params.animalName.toLowerCase()
+          collection.find({name: aliensName}).toArray()
+          .then(results => {
+              console.log(results)
+              response.json(results[0])
+          })
+          .catch(error => console.error(error))
+      })
 
 //POST 
 
-app.post('/animals', (req, res) => {
-    db.collection('animals').insertOne(req.body)
-      .then(result => {
-        res.redirect('/')
-      })
-      .catch(error => console.error(error))
+  app.post('/animals', (req, res) => {
+   db.collection('animals').insertOne(req.body)
+     .then(result => {
+      res.redirect('/')
+     })
+    .catch(error => console.error(error))
   })
 
-  //UPDATE
-  app.put('/update', (req, res)=>{
+
+    //UPDATE
+    app.put('/update', (req, res)=>{
  
-    Object.keys(req.body).forEach(key => {
-      if(req.body[key] === null || req.body[key] === undefined || req.body[key] === ''){
-        delete req.body[key]
-      }
+      Object.keys(req.body).forEach(key => {
+        if(req.body[key] === null || req.body[key] === undefined || req.body[key] === ''){
+          delete req.body[key]
+        }
+      })
+      console.log(req.body)
+      db.collection('animals').findOneAndUpdate(
+        {animalName: req.body.animalName},
+        {
+          $set: req.body
+        }
+      )
+      .then(result => {
+        console.log(result)
+        res.json('Success')
+      })
+      .catch(err => console.log(err))
     })
-    console.log(req.body)
-    db.collection('animals').findOneAndUpdate(
-      {animalName: req.body.animalName},
-      {
-        $set: req.body
-      }
-    )
-    .then(result => {
-      console.log(result)
-      res.json('Success')
-    })
-    .catch(err => console.log(err))
-  })
 
-  //Delete
+    //Delete
 
   app.delete('/delete', (req, res)=>{
     db.collection('animals').deleteOne(
@@ -93,7 +93,24 @@ app.post('/animals', (req, res) => {
     .catch(error => console.console.log(error))
   })
 
-app.listen(process.env.PORT|| PORT, ()=>{
+ app.listen(process.env.PORT|| PORT, ()=>{
     console.log(`Server running on Port ${process.env.PORT}`)
-}) 
+ }) 
 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
